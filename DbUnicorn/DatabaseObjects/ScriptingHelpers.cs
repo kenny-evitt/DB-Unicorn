c8f1d565bf706ab4007e17fd95544af06007bfed
Kenny Evitt <kevitt@recp.com>
2014-01-02 17:04:59 -0500

    Added a method for finding a stored procedure name in a Transact-SQL create script string and added a test method with several cases.
    Case 383

--- DbUnicorn/DatabaseObjects/ScriptingHelpers.cs
+++ DbUnicorn/DatabaseObjects/ScriptingHelpers.cs
@@ -1,10 +1,33 @@
 ﻿namespace DbUnicorn.DatabaseObjects
 {
+    using System;
+    using System.Text.RegularExpressions;
+
     public static class ScriptingHelpers
     {
         public static string ConvertBooleanToOnOrOffString(bool boolean)
         {
             return boolean ? "ON" : "OFF";
+        }
+
+        /// <summary>
+        /// Find the stored procedure name in a string containing a Transact-SQL 'create script'.
+        /// </summary>
+        /// <param name="createScript"></param>
+        /// <returns>A 2-tuple (pair) containing the index of the first character and the length of
+        /// the stored procedure name in the 'create script' string.</returns>
+        public static Tuple<int, int> FindStoredProcedureNameInCreateScript(string createScript)
+        {
+            Regex storedProcedureNameRegex = new Regex(@"CREATE\s+PROC(?:EDURE)?\s+(?<fullName>(?:(?<schemaName>[\p{L}_][\p{L}\p{N}@$#_]{0,127}|[""\[][^\[\]]{1,128}[""\]])\.)?(?<procedureName>[\p{L}_][\p{L}\p{N}@$#_]{0,127}|[""\[][^\[\]]{1,128}[""\]]))\s+", RegexOptions.IgnoreCase);
+
+            Match match = storedProcedureNameRegex.Match(createScript);
+
+            if (!match.Success)
+                throw new ArgumentException("No CREATE PROCEDURE statement was found in the supplied create script string.");
+
+            Group fullNameGroup = match.Groups["fullName"];
+
+            return Tuple.Create<int, int>(fullNameGroup.Index, fullNameGroup.Length);
         }
     }
 }
---
+++ Tests/ScriptingHelpersTester.cs
@@ -0,0 +1,33 @@
+﻿namespace Tests
+{
+    using System;
+    
+    using NUnit.Framework;
+
+    using DbUnicorn.DatabaseObjects;
+
+    [TestFixture]
+    public class ScriptingHelpersTester
+    {
+        private static object[] CanFindStoredProcedureNameForValidCreateScriptTestCases =
+        {
+            new object[] { "CREATE PROCEDURE ThisIsOnlyATest @parameter bit AS SELECT [@parameter] = @parameter;", Tuple.Create<int, int>(17, 15) },
+	        new object[] { "CREATE PROCEDURE [ThisIsOnlyATest] @parameter bit AS SELECT [@parameter] = @parameter;", Tuple.Create<int, int>(17, 17) },
+	        new object[] { "CREATE PROCEDURE \"ThisIsOnlyATest\" @parameter bit AS SELECT [@parameter] = @parameter;", Tuple.Create<int, int>(17, 17) },
+	        new object[] { "CREATE PROCEDURE dbo.ThisIsOnlyATest @parameter bit AS SELECT [@parameter] = @parameter;", Tuple.Create<int, int>(17, 19) },
+	        new object[] { "CREATE PROCEDURE dbo.[ThisIsOnlyATest] @parameter bit AS SELECT [@parameter] = @parameter;", Tuple.Create<int, int>(17, 21) },
+	        new object[] { "CREATE PROCEDURE dbo.\"ThisIsOnlyATest\" @parameter bit AS SELECT [@parameter] = @parameter;", Tuple.Create<int, int>(17, 21) },
+	        new object[] { "CREATE PROCEDURE [dbo].ThisIsOnlyATest @parameter bit AS SELECT [@parameter] = @parameter;", Tuple.Create<int, int>(17, 21) },
+	        new object[] { "CREATE PROCEDURE \"dbo\".ThisIsOnlyATest @parameter bit AS SELECT [@parameter] = @parameter;", Tuple.Create<int, int>(17, 21) },
+            new object[] { "CREATE PROCEDURE [dbo].[ThisIsOnlyATest] @parameter bit AS SELECT [@parameter] = @parameter;", Tuple.Create<int, int>(17, 23) },
+
+            new object[] { "create PROCEDURE [dbo].[CONTACTGROUP_MEMBERS] @CONTACTGROUPID INT", Tuple.Create<int, int>(17, 28) }
+        };
+
+        [Test, TestCaseSource("CanFindStoredProcedureNameForValidCreateScriptTestCases")]
+        public void CanFindStoredProcedureNameForValidCreateScript(string createScript, Tuple<int, int> expectedReturnValue)
+        {
+            Assert.AreEqual(expectedReturnValue, ScriptingHelpers.FindStoredProcedureNameInCreateScript(createScript));
+        }
+    }
+}
