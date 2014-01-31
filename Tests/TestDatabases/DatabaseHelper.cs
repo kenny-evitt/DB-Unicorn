376d08e17f937431bdf85cd892c4022152d5bda2
Kenny Evitt <kevitt@recp.com>
2014-01-31 13:23:54 -0500

    Added a database helper class with a single method that will be used by multiple test database classes.
    The method in this class creates a DataTable for table relationship info.
    
    Case 468

---
+++ Tests/TestDatabases/DatabaseHelper.cs
@@ -0,0 +1,36 @@
+﻿namespace Tests.TestDatabases
+{
+    using System;
+    using System.Data;
+
+    public static class DatabaseHelper
+    {
+        public static DataTable GetTableRelationshipDataTable()
+        {
+            DataTable table = new DataTable();
+
+            table.Columns.Add(
+                new DataColumn()
+                {
+                    DataType = Type.GetType("System.Int32"),
+                    ColumnName = "ObjectId"
+                });
+
+            table.Columns.Add(
+                new DataColumn()
+                {
+                    DataType = Type.GetType("System.String"),
+                    ColumnName = "SchemaName"
+                });
+
+            table.Columns.Add(
+                new DataColumn()
+                {
+                    DataType = Type.GetType("System.String"),
+                    ColumnName = "ObjectName"
+                });
+
+            return table;
+        }
+    }
+}
