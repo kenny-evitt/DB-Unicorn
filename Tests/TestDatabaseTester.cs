f3888a81845e6a17a3f3c1fe3247a45c7f6aa3cf
Kenny Evitt <kevitt@recp.com>
2014-01-31 13:25:20 -0500

    Added a test database class for code pertaining to generating foreign key relationship trees and added a class for testing the test database class code.
    Case 468

---
+++ Tests/TestDatabaseTester.cs
@@ -0,0 +1,36 @@
+﻿namespace Tests
+{
+    using NUnit.Framework;
+
+    using DbUnicorn;
+    using DbUnicorn.DatabaseObjects;
+    using Tests.TestDatabases;
+
+    [TestFixture]
+    public class TestDatabaseTester
+    {
+        [Test]
+        public void ForeignKeyRelationshipTreeDatabaseRootTableHasBackwardReference()
+        {
+            ForeignKeyRelationshipTreeDatabase database = new ForeignKeyRelationshipTreeDatabase();
+            DatabaseSchemaAndData databaseSchemaAndData = new DatabaseSchemaAndData(database);
+
+            Assert.AreEqual(1, databaseSchemaAndData.GetTableForeignKeyRelationshipReferencers(database.RootTable).Count);
+        }
+
+        [Test]
+        public void ForeignKeyRelationshipTreeDatabaseRootTableBackwardReferenceIsBackwardReferenceTable()
+        {
+            ForeignKeyRelationshipTreeDatabase database = new ForeignKeyRelationshipTreeDatabase();
+            DatabaseSchemaAndData databaseSchemaAndData = new DatabaseSchemaAndData(database);
+
+            Assert.AreEqual(
+                database.BackwardReferenceTable.Name,
+                databaseSchemaAndData.GetTableForeignKeyRelationshipReferencers(database.RootTable)[0].BaseTable.Name);
+
+            Assert.AreEqual(
+                database.BackwardReferenceTable,
+                databaseSchemaAndData.GetTableForeignKeyRelationshipReferencers(database.RootTable)[0].BaseTable);
+        }
+    }
+}
---
+++ Tests/TestDatabases/ForeignKeyRelationshipTreeDatabase.cs
@@ -0,0 +1,98 @@
+﻿namespace Tests.TestDatabases
+{
+    using System;
+    using System.Data;
+
+    using DbUnicorn;
+    using DbUnicorn.DatabaseObjects;
+    
+    public class ForeignKeyRelationshipTreeDatabase : IDatabase
+    {
+        // Private fields
+
+        private const string _schemaName = "test";
+
+        private const string _backwardReferenceTableName = "BackwardReferenceTable";
+        private const string _forwardReferenceTableName = "ForwardReferenceTable";
+        private const string _rootTableName = "RootTable";
+
+
+        // Properties
+
+        public Table BackwardReferenceTable
+        {
+            get
+            {
+                return new Table(0, new Schema(_schemaName), _backwardReferenceTableName, null);
+            }
+        }
+        
+        public Table RootTable
+        {
+            get
+            {
+                return new Table(0, new Schema(_schemaName), _rootTableName, null);
+            }
+        }
+
+        
+        // Public methods
+
+        public DataTable GetStoredProcedures()
+        {
+            throw new NotImplementedException();
+        }
+
+        public DataTable GetTable(int tableObjectId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public DataTable GetTableForeignKeyRelationshipReferencers(string schemaName, string tableName)
+        {
+            if (schemaName == _schemaName && tableName == _rootTableName)
+            {
+                DataTable references = DatabaseHelper.GetTableRelationshipDataTable();
+
+                DataRow reference = references.NewRow();
+                reference["ObjectId"] = 0;
+                reference["SchemaName"] = _schemaName;
+                reference["ObjectName"] = _backwardReferenceTableName;
+
+                references.Rows.Add(reference);
+
+                return references;
+            }
+            else
+            {
+                return new DataTable();
+            }
+        }
+
+        public DataTable GetTableForeignKeyRelationshipReferences(string schemaName, string tableName)
+        {
+            if (schemaName == _schemaName && tableName == _rootTableName)
+            {
+                DataTable references = DatabaseHelper.GetTableRelationshipDataTable();
+
+                DataRow reference = references.NewRow();
+                reference["ObjectId"] = 0;
+                reference["SchemaName"] = _schemaName;
+                reference["ObjectName"] = _forwardReferenceTableName;
+
+                references.Rows.Add(reference);
+
+                return references;
+            }
+            else
+            {
+                return new DataTable();
+            }
+        }
+
+        public DataTable GetTables()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
