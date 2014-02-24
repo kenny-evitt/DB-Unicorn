47cc46cb6120250dbb482bc91299690467d7099c
Kenny Evitt <kevitt@recp.com>
2014-02-24 11:31:31 -0500

    Added SqlBatch property to SqlBatchExecution class.
    Case 525

--- DbUnicorn/SqlBatchExecution.cs
+++ DbUnicorn/SqlBatchExecution.cs
@@ -6,7 +6,21 @@
     {
         private readonly string _sqlBatch;
 
+
+        // Properties
+
+        public string SqlBatch
+        {
+            get
+            {
+                return _sqlBatch;
+            }
+        }
+
         public SqlException SqlException { get; set; }
+
+
+        // Constructor
 
         public SqlBatchExecution(string sqlBatch)
         {
