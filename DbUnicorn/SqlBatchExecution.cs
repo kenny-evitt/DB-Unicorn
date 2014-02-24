6708411851b334a71da3db531ae764d0de75034d
Kenny Evitt <kevitt@recp.com>
2014-02-24 10:26:17 -0500

    Added class for encapsulating info about the execution of a SQL batch.
    Case 525

---
+++ DbUnicorn/SqlBatchExecution.cs
@@ -0,0 +1,16 @@
+﻿namespace DbUnicorn
+{
+    using System.Data.SqlClient;
+
+    public class SqlBatchExecution
+    {
+        private readonly string _sqlBatch;
+
+        public SqlException SqlException { get; set; }
+
+        public SqlBatchExecution(string sqlBatch)
+        {
+            _sqlBatch = sqlBatch;
+        }
+    }
+}
