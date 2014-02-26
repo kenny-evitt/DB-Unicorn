e94a3d2d53d1846a873fd5ee6ff6406c74b77dbb
Kenny Evitt <kevitt@recp.com>
2014-02-26 17:15:10 -0500

    Added initial create-database command support.
    Case 525

--- DbUnicornConsoleApp/Program.cs
+++ DbUnicornConsoleApp/Program.cs
@@ -1,9 +1,42 @@
 ﻿namespace DbUnicornConsoleApp
 {
+    using System;
+    using System.Collections.Generic;
+
+    using DbUnicorn;
+    
     class Program
     {
         static void Main(string[] args)
         {
+            string command = args[0];
+
+            switch (command)
+            {
+                case "create-database":
+                    CreateDatabase(args[1], args[2], args[3]);
+                    break;
+
+                default:
+                    throw new ApplicationException(
+                        String.Format("Command '{0}' is not recognized.", command));
+            }
+        }
+
+        private static void CreateDatabase(string serverInstanceName, string databaseName, string scriptsRootFolderPath)
+        {
+            DatabaseCreator dbCreator =
+                new DatabaseCreator(
+                    scriptsRootFolderPath,
+                    new SqlServerDatabaseServer(serverInstanceName));
+
+            IDatabase targetDatabase = dbCreator.CreateDatabase(databaseName);
+
+            List<SqlBatchExecution> batchExecutions = new List<SqlBatchExecution>();
+
+            batchExecutions.AddRange(dbCreator.CreateSchemas());
+            batchExecutions.AddRange(dbCreator.CreateUserDefinedDataTypes());
+            batchExecutions.AddRange(dbCreator.CreateTables());
         }
     }
 }
