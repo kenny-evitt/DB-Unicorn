deabf2073c6fb888bfedabcd30529216c342643d
Kenny Evitt <kevitt@recp.com>
2014-02-28 16:43:04 -0500

    Renamed class ScriptExecutor to SqlServerScriptExecutor.
    Case 577

--- DbUnicorn/DatabaseCreator.cs
+++ DbUnicorn/DatabaseCreator.cs
@@ -36,21 +36,21 @@
         {
             string schemaScriptsFolderPath = Path.Combine(_scriptsRootFolderPath, "Schemas");
 
-            ScriptExecutor.ExecuteScriptsInFolder(schemaScriptsFolderPath, _targetDatabase);
+            SqlServerScriptExecutor.ExecuteScriptsInFolder(schemaScriptsFolderPath, _targetDatabase);
         }
 
         public void CreateTables()
         {
             string tableScriptsFolderPath = Path.Combine(_scriptsRootFolderPath, "Tables");
 
-            ScriptExecutor.ExecuteScriptsInFolder(tableScriptsFolderPath, _targetDatabase);
+            SqlServerScriptExecutor.ExecuteScriptsInFolder(tableScriptsFolderPath, _targetDatabase);
         }
 
         public void CreateUserDefinedDataTypes()
         {
             string userDefinedDataTypeScriptsFolderPath = Path.Combine(_scriptsRootFolderPath, "User Defined Data Types");
 
-            ScriptExecutor.ExecuteScriptsInFolder(userDefinedDataTypeScriptsFolderPath, _targetDatabase);
+            SqlServerScriptExecutor.ExecuteScriptsInFolder(userDefinedDataTypeScriptsFolderPath, _targetDatabase);
         }
     }
 }
---
+++ DbUnicorn/SqlServerScriptExecutor.cs
@@ -0,0 +1,22 @@
+﻿namespace DbUnicorn
+{
+    using System.IO;
+
+    using TransactSqlHelpers;
+
+    public static class SqlServerScriptExecutor
+    {
+        public static void ExecuteScriptsInFolder(string folderPath, SqlServerDatabase targetDatabase)
+        {
+            foreach (string fileName in Directory.EnumerateFiles(folderPath, "*.sql"))
+            {
+                Script script = new Script(Path.Combine(folderPath, fileName));
+
+                foreach (Batch batch in script.Batches)
+                {
+                    targetDatabase.ExecuteSqlBatch(batch.Sql);
+                }
+            }
+        }
+    }
+}
